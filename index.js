cb3a64d666d65db1f75d52245840a167437b47d5
pablohernanaraujo <pablohernanaraujo@gmail.com>
2017-09-06 07:50:45 -0300

    segundo comit

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const Express = require('express');
 const app = Express();
 
+const fs = require('fs');
+const {promisify} = require('util');
+const readFile = promisify(fs.readFile);
+const im = require('gm').subClass({imageMagick: true});
+const thumbSize = 16;
+
+app.get('/', async (req, res) => {
+  let filepath = req.url;
+  if(filepath.endsWith('/'))
+    filepath += 'index.html';
+  const buffer = await readFile('./static/'+filepath);
+  const content = buffer.toString();
+  const newContent = await Promise.all(
+    content
+      .split(/(<sc-img[^>]+><\/sc-img>)/)
+      .map(async item => {
+        if(!item.startsWith('<sc-img'))
+          return item;
+        const src = /src="([^"]+)"/.exec(item)[1];
+        const img = im('./static' + src);
+        const sizeFunc = promisify(img.size.bind(img));
+        const {width, height} = await sizeFunc();
+        const thumbFunc = promisify(img.resize(thumbSize, thumbSize).toBuffer.bind(img));
+        const thumb = await thumbFunc('PNG');
+        const thumbURL = `data:image/png;base64,${thumb.toString('base64')}`;
+        return item.replace('></sc-img>', `style="padding-top: ${height/width*100}%; background-image: url(${thumbURL});"></sc-img>`);
+      })
+    );
+  res.send(newContent.join(''));
+});
+
 app.use(Express.static('static'));
 app.listen(process.env.PORT || 5000);
---
+++ static/sc-img.js
@@ -0,0 +1,70 @@
+const template = document.createElement('template');
+template.innerHTML =`
+  <style>
+    :host {
+      display: block;
+      position: relative;
+      background-size: 100% 100%;
+      margin-bottom: 18px;
+    }
+    img {
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      animation: fade-in;
+      animation-duration: 5s;
+    }
+    @keyframes fade-in {
+      from {opacity: 0}
+    }
+  </style>
+`;
+
+const io = new IntersectionObserver(entries => {
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      entry.target.setAttribute('full', '');
+    }
+  }
+});
+
+class SCImg extends HTMLElement {
+  static get observedAttributes() {
+    return ['full'];
+  }
+  constructor () {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    io.observe(this);
+  }
+
+  disconnectedCallback() {
+    io.unobserve(this);
+  }
+
+  get full() {
+    return this.hasAttribute('full');
+  }
+
+  get src() {
+    return this.getAttribute('src');
+  }
+
+  attributeChangedCallback() {
+    if (this.loaded)
+      return;
+    const img = document.createElement('img');
+    img.src = this.src;
+    img.onload = _ => {
+      this.loaded = true;
+      this.shadowRoot.appendChild(img);
+    };
+  }
+}
+
+customElements.define('sc-img', SCImg);
